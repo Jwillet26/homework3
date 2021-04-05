3f1c2068995597e33a51a69ded7bc31225a114b8
Jwillet26 <johnwillet13@gmail.com>
2021-04-05 17:31:38 -0400

    added rule for _next

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,5 @@
 const status = require('http-status-codes');
 
-// eslint-disable-next-line no-unused-vars
-module.exports = (err, req, res, next) => {
-  res.status(status.INTERNAL_SERVER_ERROR).send(`Your error was: ${err.message}`);
+module.exports = (err, req, res, _next) => {
+  res.status(status.INTERNAL_SERVER_ERROR).send(`we're sorry, your error was: ${err.message}`);
 };
